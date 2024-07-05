26bebf4c7cce8f8145bebde70b99df0aa4dc62a7
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-07-05 10:23:43 +0300

    +-test  logs

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,17 +4,21 @@ module.exports = function (req, res, next) {
   try {
     const authorizationHeader = req.headers.authorization;
     if (!authorizationHeader) {
+      // console.log('no auth header');
       return next(ApiError.UnauthorizedError());
     }
     const accessToken = authorizationHeader.split(' ')[1];
     if (!accessToken) {
+      // console.log('no access token');
       return next(ApiError.UnauthorizedError());
     }
     const userData = TokenService.validateAccessToken(accessToken);
     if (!userData) {
+      // console.log('no user data');
       return next(ApiError.UnauthorizedError());
     }
     if (!userData.isActivated) {
+      // console.log('not activated');
       return next(ApiError.ActivationError());
     }
     req.user = userData;
